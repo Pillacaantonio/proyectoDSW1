f0f5a9e50a1944f4faad6fbc2b565399b34aa30e
Pillacaantonio <129639882+Pillacaantonio@users.noreply.github.com>
2025-05-10 03:47:30 -0500

    actualizado

--- src/app/business/profile/profile.component.ts
+++ src/app/business/profile/profile.component.ts
@@ -7,17 +7,8 @@ import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { MatDialog } from '@angular/material/dialog';
 import CrearClienteComponent from './mantenimiento/crear-cliente/crear-cliente.component';
-<<<<<<< HEAD
-import {
-  state,
-  style,
-  animate,
-  transition,
-  trigger,
-} from '@angular/animations';
+import { state, style, animate, transition, trigger } from '@angular/animations';
 import { ModificarClienteComponent } from './mantenimiento/modificar-cliente/modificar-cliente.component';
-=======
->>>>>>> eab6072594cb86c948acb69437f134bfcc934e36
 
 @Component({
   selector: 'app-profile',
@@ -34,93 +25,35 @@ import { ModificarClienteComponent } from './mantenimiento/modificar-cliente/mod
   providers: [FacturaService],
   animations: [
     trigger('overlayContentAnimation', [
-      state(
-        'start',
-        style({
-          opacity: 0,
-        })
-      ),
-      state(
-        'end',
-        style({
-          opacity: 1,
-        })
-      ),
+      state('start', style({ opacity: 0 })),
+      state('end', style({ opacity: 1 })),
       transition('start <=> end', animate('300ms ease-in-out')),
     ]),
   ],
 })
 export default class ProfileComponent implements OnInit {
-<<<<<<< HEAD
   cargando: boolean = false;
   currentPage: number = 1;
-=======
- openView() {
-throw new Error('Method not implemented.');
-}
-cargando: boolean = false;
-currentPage: number = 1;
->>>>>>> eab6072594cb86c948acb69437f134bfcc934e36
   pageSize: number = 5;
   totalPages: number = 0;
   paginatedCliete: Cliente[] = [];
-  public permision!: boolean;
   mensajeError: string = '';
-
-<<<<<<< HEAD
   overlayState: string = 'start';
-
   clientes: Cliente[] = [];
   modalVisible = false;
-=======
 
->>>>>>> eab6072594cb86c948acb69437f134bfcc934e36
-
-  constructor(
-    private facturaService: FacturaService,
-    private dialog: MatDialog
-<<<<<<< HEAD
-  ) {}
-=======
-  ) { }
-
-
- openAdd() {
-  this.dialog
-      .open(CrearClienteComponent, {
-        width: '750px',
-        disableClose: true,
-      })
-      .afterClosed()
-      .subscribe((result: any) => {
-        if (result) {
-           this.obtenerListadoClientes();
-          console.log('Modal cerrado con resultado:', result);
-        }
-      });
- }
-
-  clientes: Cliente[] = [];
-  modalVisible = false;
-  overlayState = 'start';
-
->>>>>>> eab6072594cb86c948acb69437f134bfcc934e36
+  constructor(private facturaService: FacturaService, private dialog: MatDialog) {}
 
   ngOnInit(): void {
     this.obtenerListadoClientes();
   }
 
   openAdd() {
-    this.dialog
-      .open(CrearClienteComponent, {
-        width: '750px',
-        disableClose: true,
-      })
+    this.dialog.open(CrearClienteComponent, { width: '750px', disableClose: true })
       .afterClosed()
       .subscribe((result: any) => {
         if (result) {
           this.obtenerListadoClientes();
-          console.log('Modal cerrado con resultado:', result);
         }
       });
   }
@@ -130,22 +63,17 @@ currentPage: number = 1;
     this.mensajeError = '';
     this.facturaService.getListadoClientes().subscribe(
       (data) => {
-         this.clientes = data.map((cliente: { estado: number }) => ({
+        this.clientes = data.map((cliente: { estado: number }) => ({
           ...cliente,
-<<<<<<< HEAD
-          estado: cliente.estado === 1,  
-=======
-          estado: cliente.estado === 1
->>>>>>> eab6072594cb86c948acb69437f134bfcc934e36
+          estado: cliente.estado === 1,
         }));
-
-         this.totalPages = Math.ceil(this.clientes.length / this.pageSize);
-        this.updatePaginatedCliente();  
-        this.overlayState = 'end';  
-        this.cargando = false;  
+        this.totalPages = Math.ceil(this.clientes.length / this.pageSize);
+        this.updatePaginatedCliente();
+        this.overlayState = 'end';
+        this.cargando = false;
       },
       (error) => {
-        console.error('Hubo un error al obtener los clientes', error);
+        console.error('Error al obtener los clientes', error);
         this.mensajeError = 'Error al cargar clientes';
         this.cargando = false;
       }
@@ -153,39 +81,27 @@ currentPage: number = 1;
   }
 
   openEdit(cliente: any): void {
-    console.log('Cliente seleccionado para editar:', cliente);
-
-    const dialogRef = this.dialog.open(ModificarClienteComponent, {
-      data: cliente,
-    });
-
+    const dialogRef = this.dialog.open(ModificarClienteComponent, { data: cliente });
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        console.log('Cliente modificado', result);
-
         this.obtenerListadoClientes();
       }
     });
   }
+
   openView(cliente: any): void {
-    console.log('Cliente seleccionado para ver:', cliente);
-
-    const dialogRef = this.dialog.open(ModificarClienteComponent, {
-      data: { cliente, isViewMode: true },
-    });
-
+    const dialogRef = this.dialog.open(ModificarClienteComponent, { data: { cliente, isViewMode: true } });
     dialogRef.afterClosed().subscribe((result) => {
       if (result) {
-        console.log('Cliente modificado', result);
         this.obtenerListadoClientes();
       }
     });
   }
+
   eliminarCliente(id_cliente: number): void {
     if (confirm('¿Estás seguro de eliminar este cliente?')) {
       this.facturaService.eliminarCliente(id_cliente).subscribe(
         (response) => {
-          console.log('Cliente eliminado:', response);
           this.obtenerListadoClientes();
         },
         (error) => {
@@ -207,6 +123,7 @@ currentPage: number = 1;
     this.obtenerListadoClientes();
     this.hideModal();
   }
+
   updatePaginatedCliente() {
     const startIndex = (this.currentPage - 1) * this.pageSize;
     const endIndex = startIndex + this.pageSize;
